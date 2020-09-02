f88efa36deb39cc9a2506553ff58c56445a63512
Giorgiana Radoi <giorgiana.radoi@yahoo.com>
2020-09-02 18:48:15 +0300

    @schedule controller

--- src/main/java/com/grad/onlinecatalog/controller/ScheduleController.java
+++ src/main/java/com/grad/onlinecatalog/controller/ScheduleController.java
@@ -1,7 +1,9 @@
 package com.grad.onlinecatalog.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ScheduleController {
     @GetMapping("schedule")
     public String showSchedule() {
