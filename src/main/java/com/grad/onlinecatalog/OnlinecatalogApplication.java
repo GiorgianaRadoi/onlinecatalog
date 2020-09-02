987c5e8be7c3c24d8d12003c34308e38bd7b5ff1
Giorgiana Radoi <giorgiana.radoi@yahoo.com>
2020-09-02 19:53:05 +0300

    upload form

--- src/main/java/com/grad/onlinecatalog/OnlinecatalogApplication.java
+++ src/main/java/com/grad/onlinecatalog/OnlinecatalogApplication.java
@@ -1,16 +1,21 @@
 package com.grad.onlinecatalog;
 
+import com.grad.onlinecatalog.config.StorageProperties;
 import com.grad.onlinecatalog.model.Student;
 import com.grad.onlinecatalog.repository.PendingUserRepository;
 import com.grad.onlinecatalog.repository.StudentRepository;
 import com.grad.onlinecatalog.repository.UserRepository;
 import com.grad.onlinecatalog.service.SendGridEmailService;
+import com.grad.onlinecatalog.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class OnlinecatalogApplication implements CommandLineRunner {
 
 	@Autowired
@@ -29,14 +34,25 @@ public class OnlinecatalogApplication implements CommandLineRunner {
 		SpringApplication.run(OnlinecatalogApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+
+	}
+
 	@Override
-	public void run(String... args) throws Exception {
-
+	public void run (String...args) throws Exception {
 		try {
-			userRepository.deleteById(1);
+			userRepository.deleteById( 1 );
 		} catch (Exception ex) {
-
 		}
+
+	}
+
+
 		//sendGridEmailService.sendHTML("buhaidebalta.15@gmail.com", "buhaidebalta.15@gmail.com", "Hello from the other side!", "Hello!");
 //		Student student = new Student();
 //		student.setFirstName("John");
@@ -61,4 +77,6 @@ public class OnlinecatalogApplication implements CommandLineRunner {
 //		}
 
 	}
-}
+
+
+
