8f48acfcdd4140777d76e9695b91e39f1fc15e55
Giorgiana Radoi <giorgiana.radoi@yahoo.com>
2020-10-11 16:05:08 +0300

    grade list

--- src/main/java/com/grad/onlinecatalog/controller/GradeController.java
+++ src/main/java/com/grad/onlinecatalog/controller/GradeController.java
@@ -23,16 +23,20 @@ public class GradeController {
     private StudentService studentService;
 
     @GetMapping("allgrades")
-    public String showAllGrades(Model model) {
-        List<Grade> gradeList = gradeService.findAll();
-        model.addAttribute("grade", gradeList);
+    public String showAllGrades(Model model, @PathVariable Integer id) {
+
+        model.addAttribute( "students",
+                studentService.findByUnitId( id ) );
+
         return "grade/viewallgrades";
     }
 
     @GetMapping("/{id}/allgrades")//ruta care trebuie sa se regasesca in html
     public String addGrade(Model model, @PathVariable Integer id) {
         Grade grade = new Grade();
-        studentService.findAll();
+        model.addAttribute( "student",
+                studentService.findByUnitId( id ));
+        grade.setStudent( studentService.findById( id ) );
         model.addAttribute( "grade", grade );
         return "grade/viewallgrades";
     }
